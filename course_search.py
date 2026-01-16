f319584a348102611c57a8427a7e7c633edbbf94
Aaron Ormond <aormond@uvic.ca>
2026-01-16 13:45:58 -0800

    Cleaned up code

--- course_search.py
+++ course_search.py
@@ -28,21 +28,14 @@ scraper_file = ROOT / "scripts" / "course_search_scraper.js"
 
 subject = "CSC"
 
-# 1. Setup Driver
-
 if len(sys.argv) == 2:
     subject = sys.argv[1] 
 
-# starting_page = lookup.lookup(subject)
-
 try:
-    # 2. Load and Run Script
     with open(scraper_file, "r", encoding="utf-8") as f:
         scraper_js = f.read()
 
     driver.get("https://banner.uvic.ca/StudentRegistrationSsb/ssb/classSearch/classSearch")
-    
-    # print("Injecting Nav...")
     driver.execute_script("document.getElementById('classSearchLink').click();")
     driver.execute_script("""
         $('#txt_term').val('202601').trigger('change');
@@ -56,45 +49,22 @@ try:
         hiddenInput.dispatchEvent(event);
         document.getElementById('search-go').click();
     """)
-    # time.sleep(1)
-    # driver.execute_script("document.getElementById('search-go').click();")
     time.sleep(0.5)
     driver.execute_script("""
         $('.page-size-select').val('50').change();
     """)
-    time.sleep(0.5)
-    # print(f"Navigating to page {starting_page}...")
-
-    # 1. Set the page number
-    # 2. Trigger the change event
-    # 3. Simulate the 'Enter' key (keyCode 13)
-    # driver.execute_script(f"""
-    #     var pg = $('.page-number');
-    #     pg.val('{starting_page}').change();
-    #     var e = $.Event('keydown');
-    #     e.which = 13; 
-    #     e.keyCode = 13;
-    #     pg.trigger(e);
-    # """)
-
-    # IMPORTANT: Wait for the network to fetch the new page data 
-    # before you inject the scraper to start reading rows.
     time.sleep(0.8)
 
-    # time.sleep(30) # Give yourself time to click "Search"
-
-    # print("Injecting Scraper...")
     setup_subject_js = f"window.targetSubject = '{subject}';"
     driver.execute_script(setup_subject_js + scraper_js)
 
     # 3. POLLING LOOP: Wait for JS to finish
-    # print(f"Scraping pages... (Checking for {subject} courses)")
     while True:
         # Check if our global flag is set to true
         is_done = driver.execute_script("return window.scrapingFinished || false;")
         if is_done:
             break
-        time.sleep(1) # Wait 2 seconds before checking again
+        time.sleep(0.1) # Wait 1 seconds before checking again
 
     # 4. Get Data and Save
     all_data = driver.execute_script("return window.finalData;")
@@ -111,8 +81,8 @@ try:
     with open(output_path, "w", encoding="utf-8") as f:
         json.dump(all_data, f, indent=4)
 
-    print(f"Success! Saved {len(all_data)} courses to {output_path}")
+    print(f"Success! Saved {len(all_data)} courses to {output_filename}")
     print(f"Process took {time.time() - start_time:.2f} seconds")
 finally:
-    driver.quit() # Keep open to inspect, or uncomment to auto-close
+    driver.quit()
     pass
