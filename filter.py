3655bafac7de50a2d68d943446e5a7fc8eafcd8e
Aaron Ormond <aormond@uvic.ca>
2026-01-15 20:37:17 -0800

    Specific scraper script runs itself now

--- filter.py
+++ filter.py
@@ -4,25 +4,43 @@ from selenium import webdriver
 from pathlib import Path
 
 ROOT = Path(__file__).resolve().parent
-SCRAPER_FILE = ROOT / "scripts" / "specific_scraper.js"
+scraper_file = ROOT / "scripts" / "specific_scraper.js"
+# get_to_search_results_file = ROOT / "scripts" / "get_to_search_results.js"
+
 
 # 1. Setup Driver
 driver = webdriver.Chrome()
 
 try:
     # 2. Load and Run Script
-    with open(SCRAPER_FILE, "r", encoding="utf-8") as f:
-        js_content = f.read()
+    with open(scraper_file, "r", encoding="utf-8") as f:
+        scraper_js = f.read()
+    
+    # with open(get_to_search_results_file, "r", encoding="utf-8") as f:
+    #     nav_js = f.read()
 
     # NOTE: You must manually navigate to the search RESULTS page first, 
     # or add logic here to perform the search.
     driver.get("https://banner.uvic.ca/StudentRegistrationSsb/ssb/classSearch/classSearch")
     
-    print("Please perform your search in the browser now. Script will wait 10s...")
-    time.sleep(15) # Give yourself time to click "Search"
+    print("Injecting Nav...")
+    driver.execute_script("document.getElementById('classSearchLink').click();")
+    # time.sleep(1)
+    driver.execute_script("""
+        $('#txt_term').val('202601').trigger('change');
+        document.getElementById('term-go').click();
+    """)
+    time.sleep(0.5)
+    driver.execute_script("document.getElementById('search-go').click();")
+    time.sleep(1)
+    driver.execute_script("""
+        $('.page-size-select').val('50').change();
+    """)
+
+    # time.sleep(30) # Give yourself time to click "Search"
 
     print("Injecting Scraper...")
-    driver.execute_script(js_content)
+    driver.execute_script(scraper_js)
 
     # 3. POLLING LOOP: Wait for JS to finish
     print("Scraping pages... (Checking for ANTH courses)")
